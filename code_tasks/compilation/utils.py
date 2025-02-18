19ccdd6552f605adab580c7eee481ea718d19e3c
Bruches Elena <bruches@bk.ru>
2025-02-18 11:08:54 +0700

    rename

--- code_tasks/compilation/utils.py
+++ code_tasks/compilation/utils.py
@@ -1,7 +1,5 @@
 from typing import Dict, List
-import re
 
-from numpy import argmax
 
 INSTRUCTION = ("Даны два кода: фокальный и тест. Определи, скомпилируется ли тест. "
                "Верни 0, если не скомпилируется, и 1, если скомпилируется.\n{inputs}")
@@ -17,17 +15,14 @@ def doc_to_text(doc: dict) -> str:
 
 
 def process_results(doc: Dict, results: List[str]) -> Dict[str, float]:
-    """
-    Функция берет из документа (сэмпла) название домена, а также проверяет, есть
-    ли в словаре правильный ответ (для закртого теста может не быть). Если есть,
-    считаем accuracy для данного сэмпла и записываем в словарь к ключу "acc" - общая
-    accuracy, "acc.domain" - accuracy среди всех вопросов конкретного domain (которые
-    имеют одинаковое значение domain в doc["meta"]["domain"])
-    """
     has_outputs = doc['label'] is not None
     if has_outputs:
         gold = doc['label']
-        pred = 1 if '1' in results[0] else 0
+        pred = -1
+        if '1' in results[0]:
+            pred = 1
+        elif '0' in results[0]:
+            pred = 0
         # метрика для данного сэмпла и ответа модели на него
         acc = float(pred == gold)
         # сохранение в словарь общей accuracy, а также запись 0 или 1 в acc.domain
@@ -37,4 +32,3 @@ def process_results(doc: Dict, results: List[str]) -> Dict[str, float]:
         return {
             "acc": 0.0
         }
-
