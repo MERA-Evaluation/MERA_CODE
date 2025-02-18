4259bed8626d6e1493f299042f7522fa140274c1
BruchesLena <bruches@bk.ru>
2025-02-18 07:00:05 +0300

    added some fixes

--- code_tasks/reward_model/utils.py
+++ code_tasks/reward_model/utils.py
@@ -1,5 +1,7 @@
-import datasets
+from typing import Dict, List
+import re
 
+from numpy import argmax
 
 INSTRUCTION = ("Даны два кода: фокальный и тест. Определи, скомпилируется ли тест. "
                "Верни 0, если не скомпилируется, и 1, если скомпилируется.\n{inputs}")
@@ -12,4 +14,27 @@ def doc_to_text(doc: dict) -> str:
     prompt = INSTRUCTION.format(inputs=inputs)
 
     return prompt.strip()
+
+
+def process_results(doc: Dict, results: List[str]) -> Dict[str, float]:
+    """
+    Функция берет из документа (сэмпла) название домена, а также проверяет, есть
+    ли в словаре правильный ответ (для закртого теста может не быть). Если есть,
+    считаем accuracy для данного сэмпла и записываем в словарь к ключу "acc" - общая
+    accuracy, "acc.domain" - accuracy среди всех вопросов конкретного domain (которые
+    имеют одинаковое значение domain в doc["meta"]["domain"])
+    """
+    has_outputs = doc['label'] is not None
+    if has_outputs:
+        gold = doc['label']
+        pred = 1 if '1' in results[0] else 0
+        # метрика для данного сэмпла и ответа модели на него
+        acc = float(pred == gold)
+        # сохранение в словарь общей accuracy, а также запись 0 или 1 в acc.domain
+        return {"acc": acc}
+    if not has_outputs:
+        # если ответов нет, то нули
+        return {
+            "acc": 0.0
+        }
 
