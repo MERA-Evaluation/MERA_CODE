88e54af17867156b67567851a524fa22bb7b72d5
Pavel Adamenko <paadamenko@ecs-gigacode-worker-01.internal>
2025-04-17 15:33:34 +0300

    start local debugging, because of OSError

--- code_tasks/realcode_rt/some_script.sh
+++ code_tasks/realcode_rt/some_script.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+cd /data/adam/git/MERA_CODE
+source activate lm_eval
 lm_eval --model local-completions \
       --model_args model=/home/jovyan/adamenko/models/gigacode_chat_pro_v1.4_32b_coder,base_url=http://localhost:6021/v1/completions,num_concurrent=1,max_retries=3,tokenized_requests=True,max_length=2048,max_gen_toks=1024,tokenizer=Qwen/Qwen2.5-Coder-32B-Instruct \
       --tasks realcode_rt \
@@ -10,7 +12,7 @@ lm_eval --model local-completions \
       --include_path=./code_tasks \
       --apply_chat_template \
       --verbosity DEBUG \
-      --limit 1000
+      --limit 5
 # lm_eval --model local-completions \
 # --model_args model=/home/jovyan/adamenko/models/gigacode_chat_pro_v1.4_32b_coder,base_url=http://localhost:6021/v1/completions,num_concurrent=1,max_retries=3,tokenized_requests=True,max_length=2048,max_gen_toks=1024,tokenizer=Qwen/Qwen2.5-Coder-32B-Instruct \
 # --tasks realcode \
